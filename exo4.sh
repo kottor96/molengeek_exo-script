59b433bd2bcaeb8902ea4910fabfa4515e29a1e5
kottor <gardien243@gmail.com>
2025-03-07 11:38:57 +0100

    dossier poubelle

--- exo4.sh
+++ exo4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p public/{css/{footer,header,main},img,js}
-touch public/css/{footer,header,main}/style.css public/img/{img1,img2} public/js/main.js
+mkdir -p dossier_poubelle/public/{css/{footer,header,main},img,js}
+touch dossier_poubelle/public/css/{footer,header,main}/style.css dossier_poubelle/public/img/{img1,img2} dossier_poubelle/public/js/main.js
--- exo8.sh
+++ exo8.sh
@@ -1,1 +1,8 @@
-#!/bin
+#!/bin/bash
+echo "nom fichier"
+read  nom_fichier
+echo "combien d'exemplaire"
+read  nbf
+mkdir dossier_poubelle
+for i in {1.."${nbf[@]}"}
+    touch dossier_poubelle/$nom_fichier{$i}.txt
